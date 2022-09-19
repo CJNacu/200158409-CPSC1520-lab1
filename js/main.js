b71fcb25abff61f3b41f711f1312e327a28f90cf
CJNacu <78392355+CJNacu@users.noreply.github.com>
2022-09-18 23:48:11 -0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+// 1. print something to console to ensure you linked
+// your js correctly to your html.
+console.log("JS linked!")
+
+// 2. select the "about" paragraph
+var aboutElement = document.querySelector("#about")
+
+// 3. fix the typos in your file.
+
+aboutElement.innerHTML = "Our company is the best company around."
+
+// 4. Upload the file to netlify and change the site name to
+// "<your-student-id>-CPSC1520-lab1"
